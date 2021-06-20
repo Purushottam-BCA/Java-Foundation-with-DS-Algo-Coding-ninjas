c1f5bd1e2a9ac941f6e783630998bfa2966d5995
Purushottam <Pk1998.bca@gmail.com>
2021-06-20 23:57:07 +0530

    Structure of 2D array in Java

---
+++ L-12 Two Dimensional Array/1. BasicOf2dArray.java
@@ -0,0 +1,12 @@
+package twoDarray;
+
+public class BasicOf2dArray 
+{
+	public static void main(String[] args) 
+	{
+		int [][] arr2d = new int[3][4];
+		System.out.println(arr2d[2][1]);
+		arr2d[1][1] = 10;
+		System.out.println(arr2d[1][1]);
+	}
+}
